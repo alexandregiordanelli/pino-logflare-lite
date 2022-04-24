54b82c7d9f5ac557f82a2140fbf6d85830f0eb5b
Alexandre <alexandre.giordanelli@gmail.com>
2022-04-24 16:37:41 -0300

    change

--- index.mjs
+++ index.mjs
@@ -1,8 +1,7 @@
 import { sendLogBackEnd } from './utils.mjs';
 import build from 'pino-abstract-transport';
-import fetch from 'node-fetch';
 
-export default async function(logflare) {
+export default async function(fetch, logflare) {
 	return build(async function (source) {
 		for await (const obj of source) {
 			await sendLogBackEnd(fetch, logflare, obj)
